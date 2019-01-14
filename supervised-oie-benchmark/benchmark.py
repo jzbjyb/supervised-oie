a42437ba8998459e49c3501060ef4dc05f99d9fb
jzb <zhengbaj@cs.cmu.edu>
2019-01-13 22:41:17 -0500

    add error analysis

--- supervised-oie-benchmark/benchmark.py
+++ supervised-oie-benchmark/benchmark.py
@@ -23,6 +23,10 @@ from sklearn.metrics import auc
 import re
 import logging
 import pdb
+from itertools import groupby
+from termcolor import colored
+from xtermcolor import colorize
+import random
 logging.basicConfig(level = logging.INFO)
 
 from oie_readers.stanfordReader import StanfordReader
@@ -37,6 +41,58 @@ from oie_readers.goldReader import GoldReader
 from matcher import Matcher
 from operator import itemgetter
 
+SEED = 2019
+random.seed(SEED)
+np.random.seed(SEED)
+
+class ColoredExtraction(object):
+    def __init__(self, extraction):
+        self.extraction = extraction
+
+    @staticmethod
+    def color_show():
+        print(colorize('predicate color', ansi=196))
+        for i in range(5):
+            print(colorize('arg{} color'.format(i), ansi=40 + (i * 1)))
+
+    @staticmethod
+    def gen_binary(starts, ends, length):
+        pairs = list(zip(starts, ends))
+        result = []
+        for i in range(length):
+            find = False
+            for s,e in pairs:
+                if i >= s and i < e:
+                    find = True
+                    break
+            result.append(find)
+        return result
+
+    def __str__(self):
+        pred = self.extraction.elementToStr(self.extraction.pred)
+        args = [self.extraction.elementToStr(arg) for arg in self.extraction.args]
+        sent = ' {} '.format(self.extraction.sent) # add space to avoid 'on' match 'money'
+        pred_start = sent.find(' {} '.format(pred)) + 1
+        pred_end = pred_start + len(pred)
+        args_start = [sent.find(' {} '.format(arg)) + 1 for arg in args]
+        args_end = [args_start[i] + len(arg) for i, arg in enumerate(args)]
+        pred_cond = ColoredExtraction.gen_binary([pred_start], [pred_end], len(sent))
+        args_cond = [ColoredExtraction.gen_binary([arg_start], [arg_end], len(sent)) \
+            for arg_start, arg_end in zip(args_start, args_end)]
+        result = []
+        for i, ch in enumerate(sent):
+            if pred_cond[i]:
+                #ch = colored(ch, 'red')
+                ch = colorize(ch, ansi=196)
+            else:
+                for argi, arg_cond in enumerate(args_cond):
+                    if arg_cond[i]:
+                        #ch = colored(ch, 'cyan')
+                        ch = colorize(ch, ansi=40 + argi)
+                        break
+            result.append(ch)
+        return ''.join(result)[1:-1]
+
 class Benchmark:
     ''' Compare the gold OIE dataset against a predicted equivalent '''
     def __init__(self, gold_fn):
@@ -44,6 +100,81 @@ class Benchmark:
         gr = GoldReader()
         gr.read(gold_fn)
         self.gold = gr.oie
+
+    @staticmethod
+    def get_n_random_sample_out_of(samples, n):
+        return samples[np.random.permutation(len(samples))[:n]]
+
+    def get_ana_banner(self, ind):
+        result = [
+            '# ====================== #',
+            '# ===== ANALYSIS {} ===== #'.format(ind),
+            '# ====================== #']
+        return '\n'.join(result)
+
+    def error_ana_bi(self, predicted1, preddicted2):
+        pass
+
+    def error_ana_uni(self, predicted, showcase=5):
+        ColoredExtraction.color_show()
+        '''
+        ana1
+        '''
+        print(self.get_ana_banner(1))
+        sent_empty = np.array([k for k in self.gold if k not in predicted])
+        print('{} out of {} sentence have no extraction'.format(len(sent_empty), len(self.gold)))
+        cases = Benchmark.get_n_random_sample_out_of(sent_empty, showcase)
+        print('{} samples of them'.format(len(cases)))
+        for s in cases:
+            print('* {}'.format(s))
+            for e in self.gold[s]:
+                #print('\t{}'.format(e))
+                print('\t{}'.format(ColoredExtraction(e)))
+        '''
+        ana2 
+        '''
+        print(self.get_ana_banner(2))
+        wrong_ext = np.array([(s, e) for s in predicted for e in predicted[s] 
+            if len(e.matched) == 0])
+        num_ext_all = len([1 for s in predicted for e in predicted[s]])
+        num_ext_wrong = len(wrong_ext)
+        print('{} out of {} extractions are wrong'.format(num_ext_wrong, num_ext_all))
+        wrong_ext_dict = dict((k, list(e[1] for e in g)) 
+            for k, g in groupby(wrong_ext, lambda x: x[0]))
+        cases = Benchmark.get_n_random_sample_out_of(
+            np.array(list(wrong_ext_dict.items())), showcase)
+        print('{} samples of them'.format(len(cases)))
+        for s, exts in cases:
+            print('* {}'.format(s))
+            print('\t--- ground truth ---')
+            for e in self.gold[s]:
+                #print('\t{}'.format(e))
+                print('\t{}'.format(ColoredExtraction(e)))
+            print('\t--- wrong ---')
+            for e in exts:
+                #print('\t{}'.format(e))
+                print('\t{}'.format(ColoredExtraction(e)))
+        '''
+        ana3
+        '''
+        print(self.get_ana_banner(3))
+        correct_ext = np.array([(s, e) for s in predicted for e in predicted[s] 
+            if len(e.matched) > 0])
+        correct_ext_dict = dict((k, list(e[1] for e in g))
+            for k, g in groupby(correct_ext, lambda x: x[0]))
+        cases = Benchmark.get_n_random_sample_out_of(
+            np.array(list(correct_ext_dict.items())), showcase)
+        print('{} samples of correct'.format(len(cases)))
+        for s, exts in cases:
+            print('* {}'.format(s))
+            print('\t--- ground truth ---')
+            for e in self.gold[s]:
+                #print('\t{}'.format(e))
+                print('\t{}'.format(ColoredExtraction(e)))
+            print('\t--- correct ---')
+            for e in exts:
+                #print('\t{}'.format(e))
+                print('\t{}'.format(ColoredExtraction(e)))
 
     def compare(self, predicted, matchingFunc, output_fn, error_file = None):
         ''' Compare gold against predicted using a specified matching function.
@@ -55,8 +186,11 @@ class Benchmark:
 
         correctTotal = 0
         unmatchedCount = 0
-        predicted = Benchmark.normalizeDict(predicted)
-        gold = Benchmark.normalizeDict(self.gold)
+
+        # It seems that we don't need normalize
+        #predicted = Benchmark.normalizeDict(predicted)
+        #gold = Benchmark.normalizeDict(self.gold)
+        gold = self.gold
 
         for sent, goldExtractions in gold.items():
             if sent not in predicted:
@@ -123,6 +257,8 @@ class Benchmark:
             fout.write('{0}\t{1}\n'.format("Precision", "Recall"))
             for cur_p, cur_r in sorted(zip(p, r), key = lambda (cur_p, cur_r): cur_r):
                 fout.write('{0}\t{1}\n'.format(cur_p, cur_r))
+
+        return predicted
 
     @staticmethod
     def prCurve(y_true, y_scores, recallMultiplier):
@@ -243,7 +379,9 @@ if __name__ == '__main__':
     out_filename = args['--out']
 
     logging.info("Writing PR curve of {} to {}".format(predicted.name, out_filename))
-    b.compare(predicted = predicted.oie,
+    compared_predicated = b.compare(predicted = predicted.oie,
               matchingFunc = matchingFunc,
               output_fn = out_filename,
               error_file = args["--error-file"])
+    b.error_ana_uni(compared_predicated, showcase=5)
+
