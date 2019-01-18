1aec293ba19ca85b1e9f276c424e6e37714a1e27
jzb <zhengbaj@cs.cmu.edu>
2019-01-18 16:50:25 -0500

    add head extraction pipeline

--- ExtractHead.java
+++ ExtractHead.java
@@ -4,7 +4,9 @@ import edu.stanford.nlp.ie.util.*;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.semgraph.*;
 import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.simple.*;
 import java.util.*;
+import java.io.*;
 
 
 public class ExtractHead {
@@ -22,7 +24,8 @@ public class ExtractHead {
     }
   }
 
-  public static Combo topHeadFinder(Tree node, Tree parent, HeadFinder headFinder, int start, int end, List<String> went, int depth) {
+  public static Combo topHeadFinder(Tree node, Tree parent, HeadFinder headFinder, 
+    int start, int end, List<String> went, int depth) {
     if (node.isLeaf()) { // leaf node
       int ind = went.size();
       went.add(node.toString());
@@ -70,12 +73,92 @@ public class ExtractHead {
     }
   }
 
-  public static void main(String[] args) {
+  public static int[] getRange(String sentence, String subSentence) {
+    // words in sentence and subSentence are separated by one whitespace
+    int[] result = new int[2];
+    int startInd = (" " + sentence + " ").indexOf(" " + subSentence + " ");
+    if (startInd < 0) 
+      throw new IllegalArgumentException("can't find\n" + subSentence + "\nin\n" + sentence);
+    int count = 0;
+    for (int i = 0; i < startInd; i++)
+      if (sentence.charAt(i) == ' ') count++;
+    result[0] = count;
+    count = 1;
+    for (int i = 0; i < subSentence.length(); i++)
+      if (subSentence.charAt(i) == ' ') count++;
+    result[1] = result[0] + count;
+    return result;
+  }
+
+  public static void converter(String filename, String outFilename, boolean useSimple)
+    throws Exception {
+    Scanner scan = new Scanner(new File(filename));
+    PrintWriter pw = new PrintWriter(new FileWriter(outFilename));
+    CollinsHeadFinder headFinder = new CollinsHeadFinder();
+    Map<String, Tree> cache = new HashMap<String, Tree>();
+    String line = null;
+    StanfordCoreNLP coreNLP = null;
+    if (!useSimple) {
+      // initialize stanford nlp
+      Properties props = new Properties();
+      props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse");
+      coreNLP = new StanfordCoreNLP(props);
+    }
+    do {
+      // read one line
+      line = scan.nextLine();
+      List<String> data = Arrays.asList(line.split("\t"));
+      String sent = data.get(0);
+      int confidence = 1;
+      // parse the sentence
+      if (!cache.containsKey(sent)) {
+        if (useSimple) {
+          Sentence simSent = new Sentence(Arrays.asList(sent.split(" ")));
+          cache.put(sent, simSent.parse());
+        } else {
+          CoreDocument doc = new CoreDocument(sent);
+          coreNLP.annotate(doc);
+          cache.put(sent, doc.sentences().get(0).constituencyParse());
+        }
+      }
+      Tree tree = cache.get(sent);
+      // get the head for the regions we want
+      List<String> heads = new ArrayList<String>();
+      for (int i = 1; i < data.size(); i++) {
+        int[] range = getRange(sent, data.get(i));
+        List<String> went = new ArrayList<String>();
+        Combo head = topHeadFinder(
+          tree.children()[0], tree, headFinder, range[0], range[1], went, 0);
+        if (head == null) {
+          System.out.println(sent);
+          System.out.println(range[0] + " " + range[1]);
+          throw new Exception("can't find head");
+        }
+        heads.add(head.head);
+      }
+      if (heads.size() != data.size() - 1)
+        throw new IllegalArgumentException("#head not correct");
+      // output to file
+      pw.println(String.join("\t", data) + "\t<SYN_HEAD>\t" + String.join("\t", heads));
+    } while (scan.hasNext());
+    scan.close();
+    pw.close();
+  }
+
+  public static void main(String[] args) 
+    throws Exception {
+    String inFile = args[0];
+    String outFile = args[1];
+    converter(inFile, outFile, true);
+  }
+
+  public static void demo(String[] args) {
     // set up pipeline properties
     Properties props = new Properties();
     // set the list of annotators to run
     props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse");
-    // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
+    // set a property for an annotator, 
+    // in this case the coref annotator is being set to use the neural algorithm
     props.setProperty("coref.algorithm", "neural");
     // build pipeline
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
--- run_extract_head.sh
+++ run_extract_head.sh
@@ -1,2 +1,6 @@
-/home/ziyux/src/jdk1.8.0_191/bin/javac -cp "/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*" ExtractHead.java
-/home/ziyux/src/jdk1.8.0_191/bin/java -Xmx4g -cp "/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*:." ExtractHead
+javac=/home/ziyux/src/jdk1.8.0_191/bin/javac
+java=/home/ziyux/src/jdk1.8.0_191/bin/java
+$javac -cp "/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*" ExtractHead.java
+$java -Xmx4g -cp "/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*:." ExtractHead \
+	supervised-oie-benchmark/oie_corpus/test.oie.orig.correct \
+	supervised-oie-benchmark/oie_corpus/test.oie.orig.correct.head.test
