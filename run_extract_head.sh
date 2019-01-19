c9d5b6d5e6e1a3524d04118c01d77d191a6eaca5
jzb <zhengbaj@cs.cmu.edu>
2019-01-18 21:45:31 -0500

    modify scripts

--- run_extract_head.sh
+++ run_extract_head.sh
@@ -1,6 +1,16 @@
 javac=/home/ziyux/src/jdk1.8.0_191/bin/javac
 java=/home/ziyux/src/jdk1.8.0_191/bin/java
-$javac -cp "/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*" ExtractHead.java
-$java -Xmx4g -cp "/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*:." ExtractHead \
+cp=/home/zhengbaj/lib/stanford-corenlp-full-2018-02-27/*
+$javac -cp "$cp" ExtractHead.java
+$java -Xmx4g -cp "$cp:." ExtractHead \
 	supervised-oie-benchmark/oie_corpus/test.oie.orig.correct \
-	supervised-oie-benchmark/oie_corpus/test.oie.orig.correct.head.test
+	supervised-oie-benchmark/oie_corpus/test.oie.orig.correct.head &&
+$java -Xmx4g -cp "$cp:." ExtractHead \
+	external_datasets/mesquita_2013/processed/web.oie.correct \
+	external_datasets/mesquita_2013/processed/web.oie.correct.head &&
+$java -Xmx4g -cp "$cp:." ExtractHead \
+	external_datasets/mesquita_2013/processed/nyt.oie.correct \
+	external_datasets/mesquita_2013/processed/nyt.oie.correct.head &&
+$java -Xmx4g -cp "$cp:." ExtractHead \
+	external_datasets/mesquita_2013/processed/penn.oie.correct \
+	external_datasets/mesquita_2013/processed/penn.oie.correct.head
--- src/evaluate_extractions.sh
+++ src/evaluate_extractions.sh
@@ -2,11 +2,11 @@ out_dir=$1
 
 pushd ../supervised-oie-benchmark
 echo "=== oie2016 ==="
-python benchmark.py --gold=./oie_corpus/test.oie.orig.correct.head --out=/dev/null --tabbed=${out_dir}/oie2016.txt --predArgLexicalMatch
+python benchmark.py --gold=./oie_corpus/test.oie.orig.correct.head --out=/dev/null --tabbed=${out_dir}/oie2016.txt --predArgHeadMatch
 echo "=== web ==="
-python benchmark.py --gold=../external_datasets/mesquita_2013/processed/web.oie --out=/dev/null --tabbed=${out_dir}/web.txt --predArgLexicalMatch
+python benchmark.py --gold=../external_datasets/mesquita_2013/processed/web.oie.correct.head --out=/dev/null --tabbed=${out_dir}/web.txt --predArgHeadMatch
 echo "=== nyt ==="
-python benchmark.py --gold=../external_datasets/mesquita_2013/processed/nyt.oie --out=/dev/null --tabbed=${out_dir}/nyt.txt --predArgLexicalMatch
+python benchmark.py --gold=../external_datasets/mesquita_2013/processed/nyt.oie.correct.head --out=/dev/null --tabbed=${out_dir}/nyt.txt --predArgHeadMatch
 echo "=== penn ==="
-python benchmark.py --gold=../external_datasets/mesquita_2013/processed/penn.oie --out=/dev/null --tabbed=${out_dir}/penn.txt --predArgLexicalMatch
+python benchmark.py --gold=../external_datasets/mesquita_2013/processed/penn.oie.correct.head --out=/dev/null --tabbed=${out_dir}/penn.txt --predArgHeadMatch
 popd
