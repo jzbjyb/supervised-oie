ef66de761a5230a7905918b4c7ce87147369bf33
jzb <zhengbaj@cs.cmu.edu>
2019-02-28 04:38:56 -0500

    modify label.sh

--- src/label.sh
+++ src/label.sh
@@ -5,9 +5,9 @@ pushd ../supervised-oie-benchmark
 python benchmark.py --gold=./oie_corpus/test.oie.orig.correct.head --out=/dev/null \
     --tabbed=${eval_dir}/oie2016.txt --predArgHeadMatch --label=${eval_dir}/oie2016.txt.label --label_format=raw &&
 python benchmark.py --gold=../external_datasets/mesquita_2013/processed/web.oie.correct.head \
-    --out=/dev/null --tabbed=${eval_dir}/web.txt --predArgHeadMatch --label=${eval_dir}/web.txt.label --label_format=raw &&
+    --out=/dev/null --tabbed=${eval_dir}/web.txt --predArgHeadMatchExclude --label=${eval_dir}/web.txt.label --label_format=raw &&
 python benchmark.py --gold=../external_datasets/mesquita_2013/processed/nyt.oie.correct.head \
     --out=/dev/null --tabbed=${eval_dir}/nyt.txt --predArgHeadMatch --label=${eval_dir}/nyt.txt.label --label_format=raw &&
 python benchmark.py --gold=../external_datasets/mesquita_2013/processed/penn.oie.correct.head \
-    --out=/dev/null --tabbed=${eval_dir}/penn.txt --predArgHeadMatch --label=${eval_dir}/penn.txt.label --label_format=raw &&
+    --out=/dev/null --tabbed=${eval_dir}/penn.txt --predArgHeadMatchExclude --label=${eval_dir}/penn.txt.label --label_format=raw &&
 popd
