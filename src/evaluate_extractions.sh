7bd7871b15c06fbd99048260cd7fa15a66b60cf6
jzb <zhengbaj@cs.cmu.edu>
2019-04-16 15:00:31 -0400

    modify evaluation script

--- src/evaluate_extractions.sh
+++ src/evaluate_extractions.sh
@@ -3,13 +3,16 @@
 out_dir=$1
 args="${@:2}"
 
+match_nary="--predArgHeadMatch"
+match_binary="--predArgHeadMatchExclude"
+
 pushd ../supervised-oie-benchmark
 echo "=== oie2016 ==="
-python benchmark.py --gold=./oie_corpus/test.oie.orig.correct.head --out=/dev/null --tabbed=${out_dir}/oie2016.txt --predArgHeadMatch $args
+python benchmark.py --gold=./oie_corpus/test.oie.orig.correct.head --out=/dev/null --tabbed=${out_dir}/oie2016.txt ${match_nary} $args
 echo "=== web ==="
-python benchmark.py --gold=../external_datasets/mesquita_2013/processed/web.oie.correct.head --out=/dev/null --tabbed=${out_dir}/web.txt --predArgHeadMatchExclude $args
+python benchmark.py --gold=../external_datasets/mesquita_2013/processed/web.oie.correct.head --out=/dev/null --tabbed=${out_dir}/web.txt ${match_binary} $args
 echo "=== nyt ==="
-python benchmark.py --gold=../external_datasets/mesquita_2013/processed/nyt.oie.correct.head --out=/dev/null --tabbed=${out_dir}/nyt.txt --predArgHeadMatch $args
+python benchmark.py --gold=../external_datasets/mesquita_2013/processed/nyt.oie.correct.head --out=/dev/null --tabbed=${out_dir}/nyt.txt ${match_nary} $args
 echo "=== penn ==="
-python benchmark.py --gold=../external_datasets/mesquita_2013/processed/penn.oie.correct.head --out=/dev/null --tabbed=${out_dir}/penn.txt --predArgHeadMatchExclude $args
+python benchmark.py --gold=../external_datasets/mesquita_2013/processed/penn.oie.correct.head --out=/dev/null --tabbed=${out_dir}/penn.txt ${match_binary} $args
 popd
